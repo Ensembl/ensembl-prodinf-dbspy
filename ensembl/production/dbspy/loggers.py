8189f96158e2f9d3074471bccab39fd5aeb9a1fa
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2021-04-13 16:46:07 +0100

    Remove unused code

--- ensembl/production/dbspy/loggers.py
+++ ensembl/production/dbspy/loggers.py
@@ -7,49 +7,6 @@ import logging
 from ensembl.production.dbspy import config
 
 
-#  date_fmt = "%Y-%m-%d %H:%M:%S %z"
-#  default_fmt = "[%(asctime)s] %(name)-14s %(levelprefix)-9s %(message)s"
-#  access_fmt = '[%(asctime)s] %(name)-14s %(levelprefix)-9s %(client_addr)s - "%(request_line)s" %(status_code)s'
-
-#  LOGGING_CONFIG = {
-#      "version": 1,
-#      "disable_existing_loggers": False,
-#      "formatters": {
-#          "default": {
-#              "()": "uvicorn.logging.DefaultFormatter",
-#              "fmt": default_fmt,
-#              "datefmt": date_fmt,
-#          },
-#          "access": {
-#              "()": "uvicorn.logging.AccessFormatter",
-#              "fmt": access_fmt,
-#              "datefmt": date_fmt,
-#          },
-#      },
-#      "handlers": {
-#          "default": {
-#              "class": "logging.StreamHandler",
-#              "formatter": "default",
-#              "stream": "ext://sys.stderr",
-#          },
-#          "access": {
-#              "class": "logging.StreamHandler",
-#              "formatter": "access",
-#              "stream": "ext://sys.stdout",
-#          },
-#      },
-#      "loggers": {
-#          "uvicorn": {"level": config.LOG_LEVEL, "handlers": ["default"]},
-#          "uvicorn.error": {"level": config.LOG_LEVEL},
-#          "uvicorn.access": {
-#              "level": config.LOG_LEVEL,
-#              "propagate": False,
-#              "handlers": ["access"],
-#          },
-#      },
-#  }
-
-#  logging.config.dictConfig(LOGGING_CONFIG)
 log_levels = {
     'critical': logging.CRITICAL,
     'error': logging.ERROR,
@@ -57,6 +14,8 @@ log_levels = {
     'info': logging.INFO,
     'debug': logging.DEBUG,
 }
+
+
 log_level = log_levels.get(config.LOG_LEVEL, logging.INFO)
 logging.getLogger("uvicorn").setLevel(log_level)
 logger = logging.getLogger("uvicorn.error")
